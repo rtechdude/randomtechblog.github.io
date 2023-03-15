79d71ce34ba1d93b3d6adb0f4ada5839c154a236
jhudecz <justin.hudecz@hotmail.com>
2023-03-15 10:14:48 -0500

    'Updates'

--- git_commit_all.bat
+++ git_commit_all.bat
@@ -2,6 +2,8 @@
 cd "C:\Users\j\Documents\git\randomtechblog.github.io"
 git add --all
 git commit -m 'Updates'
-GIT_SSH_COMMAND='ssh -i /c/Users/j/Documents/git/rtech' git push
+rem GIT_SSH_COMMAND='ssh -i /c/Users/j/Documents/git/rtech' git push
+
+git config core.sshCommand 'ssh -i /c/Users/j/Documents/git/rtech' git push
 
 pause
